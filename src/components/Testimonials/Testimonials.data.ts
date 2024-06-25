1ddea023281ff089bf8b16df283a8afee2423879
FJMoisesRomero <francomoisesromero@hotmail.com>
2024-06-25 15:03:10 -0300

    initial commit

---
+++ src/components/Testimonials/Testimonials.data.ts
@@ -0,0 +1,44 @@
+export const dataTestimonials = [
+  {
+    id: 1,
+    name: "Constanza Gomez",
+    work: "Cliente",
+    testimonial: "Muy buena landing page SPA sobre como hacer un banco",
+    image: "./assets/profile1.png",
+  },
+  {
+    id: 2,
+    name: "Catalina Varas",
+    work: "Cliente",
+    testimonial: "Muy buena landing page SPA sobre como hacer un banco",
+    image: "./assets/profile2.png",
+  },
+  {
+    id: 3,
+    name: "Lucas Martínez",
+    work: "Cliente",
+    testimonial: "Muy buena landing page SPA sobre como hacer un banco",
+    image: "./assets/profile3.png",
+  },
+  {
+    id: 4,
+    name: "Vicente Calvo",
+    work: "Cliente",
+    testimonial: "Muy buena landing page SPA sobre como hacer un banco",
+    image: "./assets/profile4.png",
+  },
+  {
+    id: 5,
+    name: "Patricia García",
+    work: "Cliente",
+    testimonial: "Muy buena landing page SPA sobre como hacer un banco",
+    image: "./assets/profile1.png",
+  },
+  {
+    id: 6,
+    name: "Raúl Vidal",
+    work: "Cliente",
+    testimonial: "Muy buena landing page SPA sobre como hacer un banco",
+    image: "./assets/profile3.png",
+  },
+];
